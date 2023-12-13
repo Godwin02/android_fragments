3b48075879a75112b4191da1566017d2fcba9d44
Godwin02 <godwinbmenachery2024a@mca.ajce.in>
2023-12-13 13:36:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fl1,new BlankFragment()).commit();
+    }
+
+    public void click1(View view) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fl1,new BlankFragment2()).commit();
+    }
+}
